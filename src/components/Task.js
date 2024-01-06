9d5843be03d8f61c3b47575e7083b80881f7d326
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-01-06 12:04:14 +0100

    Add priority class

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import TasksContext from "../context/TasksContext";
 import { initColumns } from "../providers/initData";
 
-const Task = props=> {
+const Task = props => {
     const { user, name, info, idColumn, id, priority } = props.task
 
     const { moveTask, deleteTask } = useContext(TasksContext)
@@ -21,25 +21,25 @@ const Task = props=> {
 
     return(
         <div className="task" >
-            <div className="task__priority">{priority}</div>
+            {priority !== 'no priority' && <div className={`task__priority task__priority--${priority}`}>{priority} priority</div>}
             <h4 className="task__name">{name}</h4>
-            <h4 className="task__user">{user} </h4>
             <p className="task__description">{info}</p>
+            <h4 className="task__user">{user} </h4>
             <div className="task__buttons">
                 <button
                     onClick={handleMoveLeftClick}
                     className={`task__previous task--button ${idColumn === 1 ? "task__previous--none" : null}`}>
-                    {'<'}
+                    &#8249;
                 </button>
                 <button
                     onClick={handleDeleteClick}
                     className="task__delete task--button ">
-                    x
+                    &#x00d7;
                 </button>
                 <button
                     onClick={handleMoveRightClick}
                     className={`task__next task--button ${idColumn === initColumns.length ? "task__next--none" : null}`}>
-                    {'>'}
+                    &#8250;
                 </button>
             </div>
         </div>
