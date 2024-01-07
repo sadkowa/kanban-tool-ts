2a9d48bf23c79c87ed11354928fcae26f19709e2
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-01-07 17:35:44 +0100

    Edit init data

--- src/providers/initData.js
+++ src/providers/initData.js
@@ -9,10 +9,10 @@ const initColumns = [
     { id: 1, name: 'Appearance of the website', info: 'Define the color scheme and visual style for the webpage.', idColumn: 4, user: 'Ann Rogers', priority: 'high' },
     { id: 2, name: 'Project environment', info: "Set up the project repository and version control.", idColumn: 4, user: 'Fred Callahan', priority: 'high' },
     { id: 3, name: 'HTML structure', info: 'Develop the HTML structure for the webpage', idColumn: 4, user: 'Shelley Clarke', priority: 'no priority' },
-    { id: 4, name: 'CSS styling', info: 'Implement basic CSS styling for the webpage', idColumn: 2, user: 'Anthony Rodgers', priority: 'medium' },
+    { id: 4, name: 'CSS styling', info: 'Implement basic CSS styling for the webpage', idColumn: 3, user: 'Anthony Rodgers', priority: 'medium' },
     { id: 5, name: 'JS implement', info: 'Create and integrate necessary JavaScript functionality.', idColumn: 2, user: 'Helena Kennedy', priority: 'high' },
-    { id: 6, name: 'Testing', info: 'Test the webpage for cross-browser compatibility.', idColumn: 1, user: 'Antony Mathis', priority: 'medium' },
-    { id: 7, name: 'New features', info: 'Add new features to the project.', idColumn: 1, user: 'Antony Mathis', priority: 'low' }
+    { id: 6, name: 'Testing', info: 'Test the webpage for cross-browser compatibility.', idColumn: 1, user: 'Antony Mathis', priority: 'high' },
+    { id: 7, name: 'New features', info: 'Add new features to the project.', idColumn: 2, user: 'John Perry', priority: 'low' }
   ]
 
   export {initColumns, initTasks}
