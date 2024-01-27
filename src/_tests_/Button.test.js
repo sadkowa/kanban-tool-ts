264efada5025046ce4dfc7b45a6590f8745d8381
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-01-27 20:58:09 +0100

    Create test for Button Component

---
+++ src/_tests_/Button.test.js
@@ -0,0 +1,18 @@
+import { screen, render } from "@testing-library/react";
+import Button from "../components/Button";
+import TasksContext from "../context/TasksContext";
+
+describe('<Button/>', () => {
+    it('should render the same text as passed into prop', () => {
+
+        render(
+            <TasksContext.Provider value={{ setFormIsActive: () => { } }}>
+                <Button>Add task</Button>
+            </TasksContext.Provider>
+        )
+
+        const buttonElement = screen.getByText('Add task')
+
+        expect(buttonElement).toBeInTheDocument()
+    })
+})
