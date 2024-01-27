c9a9681e1de242719a94208dbcaa55c3a8716e83
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-01-27 20:58:52 +0100

    Add describe block in Header.test.js

--- src/_tests_/Header.test.js
+++ src/_tests_/Header.test.js
@@ -1,11 +1,14 @@
 import { render, screen } from "@testing-library/react"
 import Header from "../components/Header"
 
-it('sholud render "kanban tool" heading', async () => {
-    render(<Header />)
-    const headingElement = screen.getByRole('heading', {
-        name: /kanban tool/i
-    })
+describe('<Header>', () => {
+    it('sholud render "kanban tool" heading', async () => {
+        render(<Header />)
+        const headingElement = screen.getByRole('heading', {
+            name: /kanban tool/i
+        })
 
-    expect(headingElement).toBeInTheDocument()
+        expect(headingElement).toBeInTheDocument()
+    })
 })
+
