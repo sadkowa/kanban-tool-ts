f13fc7443a8b813fa9ea1fbe7b50eb9020991543
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-01-28 08:33:24 +0100

    Delete jest.config.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = ()=> {
-    return <h1 className="header">Kanban tool</h1>
+    return <h1 className="header">Kanban Board</h1>
 }
 
 export default Header
