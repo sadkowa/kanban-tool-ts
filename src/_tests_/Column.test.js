1ebcf207e98a72713bf8d65180d66a63523682c6
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-01-28 13:37:15 +0100

    Add tests for Column component

---
+++ src/_tests_/Column.test.js
@@ -0,0 +1,70 @@
+import { render, screen } from "@testing-library/react"
+import Column from "../components/Column"
+import TasksContext from "../context/TasksContext"
+
+const initTasks = [
+    {
+        id: 1,
+        name: 'JS implement',
+        info: 'Create and integrate necessary JavaScript functionality.',
+        idColumn: 2,
+        user: 'Helena Kennedy',
+        priority: 'high'
+    },
+    {
+        id: 2,
+        name: 'Testing',
+        info: 'Test the webpage for cross-browser compatibility.',
+        idColumn: 1,
+        user: 'Antony Mathis',
+        priority: 'high'
+    },
+    {
+        id: 3,
+        name: 'New features',
+        info: 'Add new features to the project.',
+        idColumn: 2,
+        user: 'John Perry',
+        priority: 'low'
+    }
+]
+
+const contextValues = {
+    tasks: initTasks
+}
+
+const renderComponent = value => {
+    const { Provider } = TasksContext
+
+    return render(
+        <Provider value={value}>
+            <Column item={{ name: "In progress", limit: "3", id: 2 }} />
+        </Provider>
+    )
+}
+describe('<Column>', () => {
+    it('should render heading: in progress', () => {
+        renderComponent(contextValues)
+
+        const headingElement = screen.getByRole('heading', {
+            name: /in progress/i
+        })
+
+        expect(headingElement).toBeInTheDocument()
+    })
+    it('should render paragraph containing tasks limit: 3', () => {
+        renderComponent(contextValues)
+
+        const paragraphElement = screen.getByText(/3/i)
+
+        expect(paragraphElement).toBeInTheDocument()
+    })
+    it('should render two paragraphs containing: priority', () => {
+        renderComponent(contextValues)
+
+        const headingElements = screen.getAllByText(/priority/i)
+
+        expect(headingElements.length).toBe(2)
+    })
+})
+
