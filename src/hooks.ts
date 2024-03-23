36a2406e13e73762fe01f641ed99a27bde309e87
Joanna Sadkiewicz <joanna.sadkiewicz1@gmail.com>
2024-03-23 17:57:20 +0100

    Add useCallback

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,12 +1,20 @@
-export const useStorage = () => {
-	const setItem = (name: string, data: string) =>
-		localStorage.setItem(name, JSON.stringify(data));
+import { useCallback } from "react";
+import { TaskType } from "./types/types";
 
-	const getItem = (name: string) => {
+type setItemType = (name: string, data: TaskType[]) => void;
+type getItemType = (name: string) => TaskType[] | null;
+
+export const useStorage = (): [getItemType, setItemType] => {
+	const setItem: setItemType = useCallback(
+		(name, data) => localStorage.setItem(name, JSON.stringify(data)),
+		[]
+	);
+
+	const getItem: getItemType = useCallback((name) => {
 		const storedData = localStorage.getItem(name);
 
 		return storedData !== null ? JSON.parse(storedData) : null;
-	};
-    
+	}, []);
+
 	return [getItem, setItem];
 };
